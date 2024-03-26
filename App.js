0318f2e0546c64f0f472ae61e3b76792b9c6191c
Nguyễn Đức Long <longlao159@gmail.com>
2024-03-26 10:32:52 +0700

    cau_2

--- App.js
+++ App.js
@@ -1,72 +1,147 @@
 import { Text, SafeAreaView, StyleSheet, Animated } from 'react-native';
 
 // You can import supported modules from npm
-import { React, useRef } from 'react';
+
+import { React, useRef, useEffect } from 'react';
 
 export default function App() {
-  // Cau 1 a
-  const textFaded = useRef(new Animated.Value(0)).current;
+    
 
-  const fadeAni = () => {
-    Animated.timing(textFaded, {
-      toValue: 1,
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
-  };
-  fadeAni();
+  // Cau 2 a
+  const color = useRef(new Animated.Value(0)).current;
 
-  // Cau 1 b
-  const shapePosition = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(color, {
+          toValue: 0,
+          duration: 5000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(color, {
+          toValue: 4,
+          duration: 5000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  });
+  const changeColor = color.interpolate({
+    inputRange: [0, 1, 2, 3, 4],
+    outputRange: ['green', 'red', 'purple', 'yellow', 'white'],
+  });
 
-  const changeAni = () => {
-    Animated.timing(shapePosition, {
-      toValue: 250,
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
-  };
-  changeAni();
-
-  // Cau 1 c
+  // Cau 2 b
   const position = useRef(new Animated.Value(0)).current;
+  const position1 = useRef(new Animated.Value(0)).current;
+  const position2 = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(position, {
+          toValue: 50,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(position1, {
+          toValue: 100,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(position2, {
+          toValue: 150,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  });
 
-  const springAni = () => {
-    Animated.spring(shapePosition, {
-      toValue: 100,
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
-  };
-  springAni();
+  // Cau 2 c
+  const position4 = useRef(new Animated.Value(0)).current;
+  const position5 = useRef(new Animated.Value(0)).current;
+  const position6 = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(position4, {
+          toValue: 50,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(position5, {
+          toValue: 100,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(position6, {
+          toValue: 150,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  });
   return (
     <SafeAreaView style={styles.container}>
-      // Cau 1a
-      <Animated.Text
-        style={{
-          opacity: textFaded,
-        }}>
-        {' '}
-        Have a nice day!{' '}
-      </Animated.Text>
-      // Cau 1b
+     
+      // Cau 2a
       <Animated.View
-        style={{
-          height: 50,
-          width: 50,
-          backgroundColor: 'green',
-          transform: [{ translateX: shapePosition }],
-        }}>
+        style={{ height: 50, width: 50, backgroundColor: changeColor }}>
         {' '}
       </Animated.View>
-      // Cau 1c
+      //Cau 2b
       <Animated.View
         style={{
           height: 50,
           width: 50,
           backgroundColor: 'red',
-          borderRadius: 50,
-          transform: [{ translateY: shapePosition }],
+          transform: [{ translateX: position }],
+        }}>
+        {' '}      </Animated.View>
+      <Animated.View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: 'red',
+          transform: [{ translateX: position1 }],
+        }}>
+        {' '}
+      </Animated.View>
+      <Animated.View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: 'red',
+          transform: [{ translateX: position2 }],
+        }}>
+        {' '}
+      </Animated.View>
+      // Cau 2c
+      <Animated.View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: 'red',
+          transform: [{ translateX: position4 }],
+        }}>
+        {' '}
+      </Animated.View>
+      <Animated.View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: 'red',
+          transform: [{ translateX: position5 }],
+        }}>
+        {' '}
+      </Animated.View>
+      <Animated.View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: 'red',
+          transform: [{ translateX: position6 }],
         }}>
         {' '}
       </Animated.View>
